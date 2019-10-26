953b3730eee3e492fe9207d29b2fc845eda7dbfa
Brett Suwyn <brett@suwyn.com>
2019-10-26 12:47:55 -0700

    Fix error matching branch name on Push

--- src/event/getBranchName.js
+++ src/event/getBranchName.js
@@ -1,9 +1,10 @@
-const wasBranchCreated = require('./wasBranchCreated');
+const wasPush = require('./wasPush');
 const hasPullRequest = require('./hasPullRequest');
 
 function getBranchName(payload) {
-  if (wasBranchCreated(payload)) {
-    return payload.ref;
+  if (wasPush(payload)) {
+    const headRef = /^refs\/heads\//;
+    return payload.ref.replace(headRef, '');
   } else if (hasPullRequest(payload)) {
     return payload.pull_request.head.ref;
   }
